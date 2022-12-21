627c22757e9d6eb683824e6968d0a7d74aa0e152
Kate-Vabishchevich <108225103+Kate-Vabishchevich@users.noreply.github.com>
2022-12-21 22:16:59 +0000

    Deploying to gh-pages from @ Kate-Vabishchevich/goit-js-hw-11@2843b98508817dbc03cd1954ceeb881a20929d78 🚀

--- index.ac3efe11.js
+++
@@ -1,2 +0,0 @@
-({searchForm:document.querySelector("#search-form")}).searchForm.addEventListener("submit",(function(e){e.preventDefault();e.currentTarget.elements.searchQuery.value})),fetch("https://pixabay.com/api/?key=32106886-cb1cc02bd30eab36270ed5df7&q=cat&image_type=photo&orientation=horizontal&safesearch=true").then((e=>e.json())).then(console.log);
-//# sourceMappingURL=index.ac3efe11.js.map
